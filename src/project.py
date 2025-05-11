156aa3ac129ad79cb2f0acb07725977ac064a2e6
Carlos Moreno <carmoreno6@gmail.com>
2025-05-11 17:14:16 -0500

    Defined Rule of Thirds Parameters

--- src/project.py
+++ src/project.py
@@ -1,7 +1,17 @@
-##first feature
+from PIL import Image, ImageDraw
+##first features
+def create_canvas(width,height):
+    return Image.new("RGBA", (width, height), (0, 0, 0, 0))
+
+def draw_rule_of_thirds(draw, width, height, line_color=(255, 255, 255, 128)):
+    #Vertical lines
+    draw.line([(width / 3, 0), (width / 3, height)], fill=line_color, width=2)
+    draw.line([(2 * width / 3, 0), ( 2 * width / 3 height)], fill=line_color, width=2)
+
 #rule of Thirds
 #center grids
 
 ##parameters
 #Image Modes
-#Can 
+#Canvas Size
+#line options
