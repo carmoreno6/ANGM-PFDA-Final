340dd8395a6098c6f8f13da490fa2c36916bb0da
Carlos Moreno <carmoreno6@gmail.com>
2025-05-11 17:18:29 -0500

    Center cross implemented parameters

--- src/project.py
+++ src/project.py
@@ -4,9 +4,15 @@ def create_canvas(width,height):
     return Image.new("RGBA", (width, height), (0, 0, 0, 0))
 
 def draw_rule_of_thirds(draw, width, height, line_color=(255, 255, 255, 128)):
-    #Vertical lines
+    #Vertical lines L-R
     draw.line([(width / 3, 0), (width / 3, height)], fill=line_color, width=2)
-    draw.line([(2 * width / 3, 0), ( 2 * width / 3 height)], fill=line_color, width=2)
+    draw.line([(2 * width / 3, 0), (2 * width / 3, height)], fill=line_color, width=2)
+    #Horizontal line L-R
+    draw.line([(0, height / 3), (width, height /3)], fill=line_color, width=2)
+    draw.line([(0, 2 * width / 3), (width, 2 * height / 3)], fill=line_color, width=2)
+    
+
+    
 
 #rule of Thirds
 #center grids
