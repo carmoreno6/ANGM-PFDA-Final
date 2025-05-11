c9fea1663549d3900606571a3bc500a66fbf7681
Carlos Moreno <carmoreno6@gmail.com>
2025-05-11 17:25:40 -0500

    implemented saving and file type conventions along with default filenames for minimum feature needs and testing.

--- src/project.py
+++ src/project.py
@@ -11,7 +11,10 @@ def draw_rule_of_thirds(draw, width, height, line_color=(255, 255, 255, 128)):
     draw.line([(0, height / 3), (width, height /3)], fill=line_color, width=2)
     draw.line([(0, 2 * width / 3), (width, 2 * height / 3)], fill=line_color, width=2)
     
-
+def draw_center_grid(draw, width, height, line_color=(255, 255, 255, 128)):
+    #Center Cross
+    draw.line([(width /2, 0), (width / 2, height)], fill=line_color, width=2)
+    draw.line([(0, height / 2), (width, height /2)], fill=line_color, width=2)
     
 
 #rule of Thirds
